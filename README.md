0bba125ffacfb0d4a9831f76f5ad7f9fe897ca25
ivan <morphizm@ya.ru>
2019-08-24 14:17:39 +0300

    act 5

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/a174313e685af6d7e7a8/maintainability)](https://codeclimate.com/github/morphizm/frontend-project-lvl1/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/a174313e685af6d7e7a8/test_coverage)](https://codeclimate.com/github/morphizm/frontend-project-lvl1/test_coverage)
 [![Build Status](https://travis-ci.org/morphizm/frontend-project-lvl1.svg?branch=master)](https://travis-ci.org/morphizm/frontend-project-lvl1)
+
+## brain-even
+
+[![asciicast](https://asciinema.org/a/6hB3Pr6rCxNOz6jE0J9ysMMsv.svg)](https://asciinema.org/a/6hB3Pr6rCxNOz6jE0J9ysMMsv)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,45 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+export const brainGames = () => {
   console.log('Welcome to the Brain Games!');
 
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
+};
+
+const generateRandomNumber = (interval = 100) => {
+  const simpleRandom = Math.floor(Math.random() * interval);
+  return simpleRandom;
+};
+
+export const brainEven = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+
+  const iter = (trueAnswers) => {
+    if (trueAnswers === 3) {
+      console.log(`Congratulations, ${userName}!`);
+      return;
+    }
+    const num = generateRandomNumber();
+    console.log(`Question: ${num}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const isEven = num % 2 === 0;
+    if (userAnswer === 'yes' && isEven) {
+      console.log('Correct!');
+      iter(trueAnswers + 1);
+      return;
+    }
+    if (userAnswer === 'no' && !isEven) {
+      console.log('Correct!');
+      iter(trueAnswers + 1);
+      return;
+    }
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${isEven ? 'yes' : 'no'}.'`);
+    console.log(`Let's try again, ${userName}`);
+  };
+  const startCorrectUserAnswer = 0;
+  return iter(startCorrectUserAnswer);
 };
