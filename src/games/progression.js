6f1248793715959a9171b7ad050d63b0fbea4ec2
ivan <morphizm@ya.ru>
2019-08-29 15:18:31 +0300

    bug fix

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,10 +1,10 @@
 import run from '..';
 import generateRandomNumber from '../utils';
 
-const generateProggression = (start, step) => {
+const generateProggression = (start, step, lengthProgr) => {
   const iter = (progression) => {
     const len = progression.length;
-    if (len === 10) {
+    if (len === lengthProgr) {
       return progression;
     }
     return iter([...progression, progression[len - 1] + step]);
@@ -18,7 +18,8 @@ export default () => {
   const generateGameData = () => {
     const stepForProgression = generateRandomNumber(1, 30);
     const startForProgression = generateRandomNumber();
-    const progression = generateProggression(startForProgression, stepForProgression);
+    const lengthOfProgression = 10;
+    const progression = generateProggression(startForProgression, stepForProgression, lengthOfProgression);
     const hiddenElementIndex = generateRandomNumber(0, progression.length - 1);
     const correctAnswer = progression[hiddenElementIndex];
     progression[hiddenElementIndex] = '...';
